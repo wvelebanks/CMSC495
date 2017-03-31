180e30db9bac1d75782c979e4b7a5d7acc58823f
shadowx787 <shadowx787@gmail.com>
2017-03-31 00:05:48 -0400

    Update JavaDB.java
    Added in test functions to create digital signature.  Was able to sign a simple string and verify that the signature was legit.

--- testing/JavaDB.java
+++ testing/JavaDB.java
@@ -1,15 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javadb;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
-import javax.net.ssl.*;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  *
  * @author Justin
@@ -23,13 +30,13 @@ public class JavaDB {
             System.setProperty("javax.net.ssl.keyStorePassword","P@$$w0rd");
             System.setProperty("javax.net.ssl.trustStore","C:/Users/Justin/Desktop/mysql/pk_certs/truststore");
             System.setProperty("javax.net.ssl.trustStorePassword","P@$$w0rd");
-        } catch (Exception ex) {
-            // handle the error
+        } 
+        catch (ClassNotFoundException ex) {
         }
         
         Connection conn = null;
         String username = "root";
-        String password = "P@$$w0rd#787!$";
+        String password = "password";
         String dbURL = "jdbc:mysql://localhost/mysql"
                 + "?verifyServerCertificate=true"
                 + "&clientCertificateKeyStoreUrl=file:///C:/Users/Justin/Desktop/mysql/pk_certs/keystore"
@@ -49,6 +56,11 @@ public class JavaDB {
                 while(rs.next()) {
                     System.out.println(rs.getString(1));
                 }
+                
+                // TEST THE SIGNING AND VERIFYING FEATURES
+                sign("test");
+                verifySignature("test");
+                // END TEST
             }
             catch (SQLException ex){
                 // handle any errors
@@ -79,5 +91,110 @@ public class JavaDB {
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
         }
-    }    
+    }
+    
+    public static byte[] hashTimesheet(String testString) {
+        byte[] thehash = null;
+        try{
+            //String testString = "test";
+            String key = "1234";
+
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secretKey);
+            thehash = sha256_HMAC.doFinal(testString.getBytes());
+        } 
+        catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            //handle the exception
+        }
+        return thehash;
+    }
+    
+     private static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        return result.toString();
+    }
+     
+    public static KeyPair loadKey() {
+        KeyPair kp = null;
+        try{
+            KeyStore ks = KeyStore.getInstance("JKS");
+            InputStream readStream = new FileInputStream("C:/Users/Justin/Desktop/mysql/pk_certs/pk12_store/client.keystore");
+            char[] pass_array = "P@$$w0rd".toCharArray();
+            ks.load(readStream, pass_array);
+            kp = getKeyPair(ks, "jrmullins", pass_array);
+        }
+        catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+        } 
+        return kp;
+    }
+    
+    //Got code from here: https://blogs.oracle.com/weblogic-config-runtime/entry/a_short_utility_program_to
+    public static KeyPair getKeyPair(KeyStore keystore, String alias, char[] password) {
+        try {
+          // Get private key
+          Key key = keystore.getKey(alias, password);
+          if (key instanceof PrivateKey) {
+            // Get certificate of public key
+              java.security.cert.Certificate cert = keystore.getCertificate(alias);
+            // Get public key
+            PublicKey publicKey = cert.getPublicKey();
+            // Return a key pair
+            return new KeyPair(publicKey, (PrivateKey)key);
+          }
+        } 
+        catch (UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException e) {
+        }
+        return null;
+    }
+    
+    // Motly taken from here: https://kodejava.org/how-to-create-a-digital-signature-and-sign-data/
+    public static void sign(String signme) {
+        try{
+            // Get an instance of Signature object and initialize it.
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initSign(loadKey().getPrivate());
+
+            // Supply the data to be signed to the Signature object
+            // using the update() method and generate the digital
+            // signature.
+            
+            signature.update(hashTimesheet(signme));
+            byte[] digitalSignature = signature.sign();
+            
+            // Save digital signature and the public key to a file (will need to save to database).
+            Files.write(Paths.get("signature"), digitalSignature);
+            Files.write(Paths.get("publickey"), loadKey().getPublic().getEncoded());
+        }
+        catch (IOException | InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
+        }
+    }
+    
+    // Mostly taken from here: https://kodejava.org/how-to-verify-digital-signature-of-a-signed-data/
+    public static void verifySignature(String verifyme) {
+        try {
+            // WIll need to grab this from database instead of file
+            byte[] publicKeyEncoded = Files.readAllBytes(Paths.get("publickey"));
+            byte[] digitalSignature = Files.readAllBytes(Paths.get("signature"));
+
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyEncoded);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initVerify(publicKey);
+
+            signature.update(hashTimesheet(verifyme));
+
+            boolean verified = signature.verify(digitalSignature);
+            if (verified) {
+                System.out.println("Data verified.");
+            } else {
+                System.out.println("Cannot verify data.");
+            }
+        } 
+        catch (IOException | InvalidKeyException | NoSuchAlgorithmException | SignatureException | InvalidKeySpecException e) {
+        }
+    }
 }
