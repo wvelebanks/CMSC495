df0945b77edec893e0fc8ce14b9717bd07dd2c85
epetrosy18 <epetrosy@gmail.com>
2017-04-13 09:17:36 -0700

    Add files via upload

---
+++ Reference Code/departmentLibrary/Login.java
@@ -0,0 +1,41 @@
+package departmentLibrary;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/Login", loadOnStartup = 1)
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;   
+   	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		//calls the javascript page
+		request.getRequestDispatcher( "/WEB-INF/Login.jsp" ).forward(request, response );		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		//post process returned by the javascript page
+		//in this case to do a simple validation of user credentials
+		//our project will do the check against the database here instead
+		if( request.getParameter( "username" ).equals( "epetrosy" )
+	            && request.getParameter( "password" ).equals( "abcd" ) )
+	        {
+	            //set the session username
+				request.getSession().setAttribute( "username", "epetrosy" );
+				//redirects to the next java servlet program (not inluded)
+	            response.sendRedirect( "DepartmentLibraryServlet" );
+	        }
+	        else
+	            response.sendRedirect( "Login" );
+	}
+
+}
---
+++ Reference Code/departmentLibrary/README.txt
@@ -0,0 +1,13 @@
+This is just a simple program that shows how the servlet calls the javascript file. 
+
+I've annotated the files with some comments. 
+
+Please note that the code DOES NOT RUN. This because this is just a snippit of a larger program, and because we
+do not have a server set up. When I get the server up and running I will upload the whole program for reference.
+
+In the mean time, hopefully this helps you guys see the MVC architecture and the seperation between the
+model (java object), view (jsp page), and control (java servlet).
+
+-Elvin
+
+
