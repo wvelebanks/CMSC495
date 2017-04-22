9d87c77d8ca903d3ce82c2258ffb02e74e41dab9
wvelebanks <wendy.velasquezebanks@gmail.com>
2017-04-22 19:10:46 -0400

    Update ClockIn.java

--- Java/ClockIn.java
+++ Java/ClockIn.java
@@ -1,1 +1,6 @@
-Stub
+
+
+
+//here is an example: http://alvinalexander.com/java/java-timestamp-example-current-time-now
+// geeting time stamp: http://stackoverflow.com/questions/8345023/need-to-get-current-timestamp-in-java
+// this one is the simplest... https://www.mkyong.com/java/how-to-get-current-timestamps-in-java/
