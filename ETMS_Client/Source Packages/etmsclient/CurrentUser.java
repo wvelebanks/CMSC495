01d73b3b419ddb12ad1e5c94a6c62906ef000007
shadowx787 <shadowx787@gmail.com>
2017-05-02 22:08:53 -0400

    Update CurrentUser.java

--- ETMS_Client/Source Packages/etmsclient/CurrentUser.java
+++ ETMS_Client/Source Packages/etmsclient/CurrentUser.java
@@ -1,8 +1,16 @@
 package etmsclient;
 
+import java.util.Arrays;
+
 /**
+ * The CurrentUser class creates a CurrentUser object which holds values of the
+ * user who is currently logged into the ETMS Client software. It holds the
+ * user's EmployeeID, PositionID, and if the user is a supervisor the
+ * EmployeeID's of his subordinates. This class is meant to help keep track of
+ * who is logged into the ETMS client and who's information they should be able
+ * to access.
  *
- * @author Justin
+ * @author Justin Mullins
  */
 public class CurrentUser {
 
@@ -10,34 +18,99 @@ public class CurrentUser {
     int position;
     int[] employees;
 
+    /**
+     * This Constructor is for non-supervisory PositionIDs.
+     *
+     * @param userID - the current user's EmployeeID
+     * @param positionID - the current user's PositionID
+     */
     public CurrentUser(int userID, int positionID) {
         user = userID;
         position = positionID;
+        employees = new int[0];
     }
 
+    /**
+     * This Constructor is for supervisory PositionIDs.
+     *
+     * @param userID - the current user's EmployeeID
+     * @param positionID - the current user's PositionID
+     * @param employeesID - the EmployeeID's of the supervisors subordinates
+     */
     public CurrentUser(int userID, int positionID, int[] employeesID) {
         user = userID;
         position = positionID;
         employees = employeesID;
     }
 
-    public void setUser(int userID) {
-        user = userID;
+    /**
+     * Set the CurrentUser's EmployeeID
+     * 
+     * @param user - the EmployeeID
+     */
+    public void setUser(int user) {
+        this.user = user;
     }
 
+    /**
+     * Return the CurrentUser's EmployeeID
+     * 
+     * @return an integer representing the user's EmployeeID
+     */
     public int getUser() {
         return user;
     }
 
-    public void setPosition(int positionID) {
-        position = positionID;
+    /**
+     * Set the CurrentUsr's PostionID
+     * 
+     * @param position - the PositionID
+     */
+    public void setPosition(int position) {
+        this.position = position;
     }
 
+    /**
+     * Return the CurrentUser's PositionID
+     * 
+     * @return an integer representing the user's PositionID
+     */
     public int getPosition() {
         return position;
     }
 
-    public void setEmployees(int[] employeesID) {
-        employees = employeesID;
+    /**
+     * Set the CurrentUser's list of employees EmployeeID numbers
+     * 
+     * @param employees - an integer array representing the user's list of employees
+     * EmployeeID's
+     */
+    public void setEmployees(int[] employees) {
+        this.employees = employees;
+    }
+    
+    /**
+     * Return the CurrentUser's list of employees EmployeeID numbers
+     * 
+     * @return an integer array representing the user's list of employee EmployeeID's
+     */
+    public int[] getEmployees() {
+        return employees;
+    }
+
+    /**
+     * Returns a String representation of the Employee object
+     * 
+     * @return a String that shows the values of CurrentUser
+     */
+    @Override
+    public String toString() {
+        if(employees.length == 0) {
+            return "CurrentUser{" + "EmployeeID=" + user + "PositionID=" + position + "}";  
+        }
+        else {
+            return "CurrentUser{" + "EmployeeID=" + user + "PositionID=" + position + "EmployeeList="
+                    + Arrays.toString(employees) + "}";
+        }
     }
 }
