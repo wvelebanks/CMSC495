f65a0999d6f145bb5972ab080184f74a121cb851
shadowx787 <shadowx787@gmail.com>
2017-05-05 00:51:22 -0400

    Create Error.java

---
+++ testing/Justin Complete --with DB/Source Packages/etmsclient/Error.java
@@ -0,0 +1,22 @@
+
+package etmsclient;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *  @date created 
+ *  @author Ian Spooner
+ */
+
+public class Error {
+static JFrame popup = new JFrame("Popup Window");
+public static void warning(String error){
+        JOptionPane.showMessageDialog(popup,
+        "That doesn't seem to be valid... \n"
+        + error,
+        "Try again",
+        JOptionPane.WARNING_MESSAGE);
+    }
+
+}
