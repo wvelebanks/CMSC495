93c2032e020882648bfcc59093ceddedb7ad70cf
Ian Spooner <cawwot@gmail.com>
2017-05-07 08:56:04 -0400

    Update PunchTime.java
    fixed some comments

--- testing/Justin Complete --with DB/Source Packages/etmsclient/PunchTime.java
+++ testing/Justin Complete --with DB/Source Packages/etmsclient/PunchTime.java
@@ -40,55 +40,81 @@ public class PunchTime {
     }
 
     //---------getters and setters, no args--------//
+    
+    //gets the int employee ID
+    //@return employeeID
     public int getEmployeeID() {
         return employeeID;
     }
 
+    //sets the int employee ID
+    //@params int employeeID
     public void setEmployeeID(int employeeID) {
         this.employeeID = employeeID;
     }
 
+    //sets the time punch UUID
+    //@return int timeID
     public int getTimeID() {
         return timeID;
     }
 
+    //sets the UUID time punch ID, used by clock in and out classes
+    //@params int timeID
     public void setTimeID(int timeID) {
         this.timeID = timeID;
     }
 
+    //gets the individual punch in time
+    //@return String timeIn
     public String getTimeIn() {
         return timeIn;
     }
 
+    //sets the individual punch in time
+    //@params String timeIn
     public void setTimeIn(String timeIn) {
         this.timeIn = timeIn;
     }
 
+    //gets the individual punch out time
+    //@return String timeOut
     public String getTimeOut() {
         return timeOut;
     }
 
+    //sets the individual punch out time
+    //@params String timeOut
     public void setTimeOut(String timeOut) {
         this.timeOut = timeOut;
     }
 
+    //gets the individual punch out date
+    //@return String clockDate
     public String getClockDate() {
         return clockDate;
     }
 
+    //sets the individual punch out date
+    //@params String clockDate
     public void setClockDate(String clockDate) {
         this.clockDate = clockDate;
     }
 
+    //boolean flag for determining whether the punch in is paired with a punch out
+    //@return boolean completed
     public boolean isCompleted() {
         return completed;
     }
 
+    //sets the boolean flag for a paired punch
+    //@params boolean completed
     public void setCompleted(boolean completed) {
         this.completed = completed;
     }
 
     @Override
+    //generic toString, not used so far
     public String toString() {
         return "Time{" + "employeeID=" + employeeID + ", timeID=" + timeID + ", timeIn=" + timeIn + ", timeOut=" + timeOut + ", clockDate=" + clockDate + ", completed=" + completed + '}';
     }
